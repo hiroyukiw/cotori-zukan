e8bd8bab9768ff53211f364e938469101d0b9373
若宮寛幸 <h.wakamiya@corp.acall.jp>
2021-04-24 22:01:53 +0900

    create_nuxt_project

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-app-bar absolute class="d-flex justify-center align-center">
+      <div>小鳥図鑑</div>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-row>
+    <p>小鳥図鑑</p>
+  </v-row>
+</template>
